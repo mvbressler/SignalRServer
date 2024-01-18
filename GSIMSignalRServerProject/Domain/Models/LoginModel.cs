df674e17c770249f98b48f1b9a52ccd92063bab4
bresler <bresler@aleph.engineering>
2024-01-17 22:01:54 -0500

    Modify LoginModel
    Adding JsonPropertyName to each property

--- GSIMSignalRServerProject/Domain/Models/LoginModel.cs
+++ GSIMSignalRServerProject/Domain/Models/LoginModel.cs
@@ -1,8 +1,13 @@
-﻿namespace GSIMSignalRServerProject.Domain.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace GSIMSignalRServerProject.Domain.Models;
 
 public class LoginModel
 {
+    [JsonPropertyName("server")]
     public string Server { get; set; } = "127.0.0.1";
+    [JsonPropertyName("login")]
     public string? Username { get; set; }
+    [JsonPropertyName("password")]
     public string? Password { get; set; }
 }

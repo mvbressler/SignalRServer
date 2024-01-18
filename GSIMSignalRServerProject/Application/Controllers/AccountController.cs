7cb9345f23b3199aa3ed516ce61ba339982c9623
bresler <bresler@aleph.engineering>
2024-01-17 21:58:42 -0500

    Create AccountController
    Responsible for the logout simulation

---
+++ GSIMSignalRServerProject/Application/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GSIMSignalRServerProject.Application.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/account")]
+public class AccountController: ControllerBase
+{
+    [HttpGet("logout")]
+    public IActionResult Logout()
+    {
+        // You can retrieve the user's information from the token if needed
+        var userName = User.Identity?.Name;
+
+        // Perform server-side actions, such as logging the logout event
+        LogUserLogout(userName);
+
+        return Ok("Logged out successfully.");
+    }
+    
+    private void LogUserLogout(string userName)
+    {
+        Console.WriteLine($"The username {userName} is logged out successfully");
+    }
+}

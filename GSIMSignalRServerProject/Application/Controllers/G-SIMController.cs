f946c8f3d107ad7718e1cb0e9fd2813ff53aa6fe
bresler <bresler@aleph.engineering>
2024-01-17 21:59:30 -0500

    Create G-SIMController
    Will be the responsible for submitting data

---
+++ GSIMSignalRServerProject/Application/Controllers/G-SIMController.cs
@@ -0,0 +1,32 @@
+﻿using GSIMSignalRServerProject.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GSIMSignalRServerProject.Application.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/v1/g-sim")]
+public class GSimController: ControllerBase
+{
+    [HttpPost("types")]
+    public IActionResult GetToken([FromBody] List<TypesRequest>? types)
+    {
+        Console.WriteLine("Types received");
+        return Ok("Types received");
+    }
+    
+    [HttpPost("type-description")]
+    public IActionResult GetToken([FromBody] TypeDescriptionRequest? typeDescription)
+    {
+        Console.WriteLine("TypeDescriptionRequest received");
+        return Ok();
+    }
+    
+    [HttpPost("objects")]
+    public IActionResult GetToken([FromBody] List<LenelObjectsRequest>? list)
+    {
+        Console.WriteLine("ObjectsRequests received");
+        return Ok();
+    }
+}
